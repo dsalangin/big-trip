6679226387db394a6b7a23b3f791d21dd1b20ec9
dalangin <dsalangin@74.ru>
2023-10-01 20:51:16 +0300

    Configuration fix

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  // mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/dist/'
+  },
+  devServer: {
+    client: {
+      overlay: true,
+    },
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: `babel-loader`, },
+    ],
+  },
+  devtool: `eval-source-map`,
+};
